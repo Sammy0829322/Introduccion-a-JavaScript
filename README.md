c56b2f46939944342dbcefa555c5c3c0a43eeca0
samuel <friassamuel0829@gmail.com>
2026-04-30 17:28:42 -0600

    actividad-clase1

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript desde cero
+
+En este repositorio se ajunta las activiades realizadas en las diferentes lecciones de la unidad JavaScript desde cero :
+-  Intruccción a JavaScript
+   - Tipos de datos: string, number, boolean, object, undefined etc.
+   - Buenas practicas en JavaScript.
+
---
+++ tipos-de-datos.js
@@ -0,0 +1,21 @@
+// Tipos de datos en JavaScript
+console.log(typeof 42);
+console.log(typeof -67);
+console.log(typeof true);
+console.log(typeof "hola mundo! ");
+console.log(typeof 23.86);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof "cuarenta y siete");
+
+// Operacion en JavaScript
+ var numero1 =35;
+ var numero2 =45;
+
+ var resultado = numero1*numero2;
+console.log(typeof resultado);
+
+
+
